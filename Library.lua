7936421f5a13151acc88f73579e54778c55e61c5
Nol1 <zhangyuguozhang4@gmail.com>
2026-03-12 03:37:20 +0800

    Add files via upload

--- Library.lua
+++ Library.lua
@@ -190,16 +190,16 @@ local Library = {
 
     IsLightTheme = false,
     Scheme = {
-              BackgroundColor = Color3.fromRGB(15, 15, 15),
-        MainColor = Color3.fromRGB(25, 25, 25),
-        AccentColor = Color3.fromRGB(125, 85, 255),
-        OutlineColor = Color3.fromRGB(40, 40, 40),
-        FontColor = Color3.new(1, 1, 1),
-        Font = Font.fromEnum(Enum.Font.Code),
+             BackgroundColor = Color3.fromRGB(18, 18, 18),
+MainColor = Color3.fromRGB(30, 30, 30),
+AccentColor = Color3.fromRGB(140, 100, 255),
+OutlineColor = Color3.fromRGB(45, 45, 45),
+FontColor = Color3.fromRGB(230, 230, 230),
+Font = Font.fromEnum(Enum.Font.Code),
 
-        Red = Color3.fromRGB(255, 50, 50),
-        Dark = Color3.new(0, 0, 0),
-        White = Color3.new(1, 1, 1),
+Red = Color3.fromRGB(255, 70, 70),
+Dark = Color3.fromRGB(5, 5, 5),
+White = Color3.fromRGB(245, 245, 245),
     },
 
     Registry = {},
@@ -7856,13 +7856,13 @@ function Library:CreateWindow(WindowInfo)
     end
 
     if Library.IsMobile then
-        local ToggleButton = Library:AddDraggableButton("显示/隐藏", function()
+        local ToggleButton = Library:AddDraggableButton("ON/OFF", function()
             Library:Toggle()
         end)
 
-        local LockButton = Library:AddDraggableButton("锁定", function(self)
+        local LockButton = Library:AddDraggableButton("Lock", function(self)
             Library.CantDragForced = not Library.CantDragForced
-            self:SetText(Library.CantDragForced and "解锁" or "锁定")
+            self:SetText(Library.CantDragForced and "Unlock" or "Lock")
         end)
 
         if WindowInfo.MobileButtonsSide == "Right" then
